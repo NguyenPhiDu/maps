38c64bc4431e8896587ab8278c2f9c3080b9581f
NguyenPhiDu <nguyendu1249@gmail.com>
2022-03-09 23:23:14 +0700

    update seach location

--- App.js
+++ App.js
@@ -1,25 +1,32 @@
-import * as React from "react"
-import { Dimensions, StyleSheet, Text, View } from "react-native"
+import React, { useState } from "react"
+import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from "react-native"
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete"
-import MapView, { Callout, Circle, Marker } from "react-native-maps"
+import MapView, { Callout, Circle, Marker, Polyline, PROVIDER_GOOGLE } from "react-native-maps"
+import MapViewDirections from 'react-native-maps-directions';
 
 export default function App() {
-  const [pin, setPin] = React.useState({
-    latitude: 10.85185385576466,
-    longitude: 106.75793529493423
-  })
-  const [region, setRegion] = React.useState({
+
+  const [home, setHome] = useState({
     latitude: 10.85185385576466,
     longitude: 106.75793529493423,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421
-  })
+  });
+  const [company, setCompany] = useState({
+    latitude: 0,
+    longitude: 0,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421
+  });
 
   return (
-    <View style={{ paddingTop: 50, flex: 1 }}>
+    <View style={styles.container}>
       <GooglePlacesAutocomplete
-        style={{ height: 30 }}
-        placeholder="Search"
+        styles={{
+          container: { flex: 0, position: "absolute", width: "100%", zIndex: 1 },
+          listView: { backgroundColor: "white" }
+        }}
+        placeholder="Nhập điểm bắt đầu"
         fetchDetails={true}
         GooglePlacesSearchQuery={{
           rankby: "distance"
@@ -27,57 +34,114 @@ export default function App() {
         onPress={(data, details = null) => {
           // 'details' is provided when fetchDetails = true
           console.log(data, details)
-          setRegion({
-            latitude: details.geometry.location.lat,
-            longitude: details.geometry.location.lng,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
-          })
+          setHome(
+            {
+              // id: 1,
+              // latitude: 10.80368323371993,
+              // longitude: 106.74768092713525,
+              latitude: details.geometry.location.lat,
+              longitude: details.geometry.location.lng,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421
+            })
+          // console.log(details.geometry.location.lat)
+          // console.log(company.latitude)
         }}
         query={{
-          key: "AIzaSyDkbGaQqjBkL1EuWPPFUGa01Fji8Po9BKs",
-          language: "en",
-          components: "country:us",
+          key: 'AIzaSyBlduVD6U_I2cpDHlcOWzqIippfMdDSoZ8',
+          language: "vi",
+          // components: "country:us",
           types: "establishment",
           radius: 30000,
-          location: `${region.latitude}, ${region.longitude}`
+          location: `${home.latitude}, ${home.longitude}`
         }}
-        styles={{
-          container: { flex: 0, position: "absolute", width: "100%", zIndex: 1 },
-          listView: { backgroundColor: "white" }
-        }}
+        listViewDisplayed='auto'    // true/false/undefined
       />
+      <View style={{
+        marginTop: 50, width: '100%'
+      }}>
+        <GooglePlacesAutocomplete
+          styles={{
+            container: { flex: 0, position: "absolute", width: "100%", zIndex: 1 },
+            listView: { backgroundColor: "white" }
+          }}
+          placeholder="nhập điểm đến"
+          fetchDetails={true}
+          GooglePlacesSearchQuery={{
+            rankby: "distance"
+          }}
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            console.log(data, details)
+            setCompany(
+              {
+                // id: 1,
+                // latitude: 10.80368323371993,
+                // longitude: 106.74768092713525,
+                latitude: details.geometry.location.lat,
+                longitude: details.geometry.location.lng,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421
+              })
+            // console.log(details.geometry.location.lat)
+            // console.log(company.latitude)
+          }}
+          query={{
+            key: 'AIzaSyBlduVD6U_I2cpDHlcOWzqIippfMdDSoZ8',
+            language: "vi",
+            // components: "country:us",
+            types: "establishment",
+            radius: 30000,
+            location: `${company.latitude}, ${company.longitude}`
+          }}
+          listViewDisplayed='auto'    // true/false/undefined
+        />
+      </View>
       <MapView
         style={styles.map}
         initialRegion={{
-          latitude: 10.85185385576466,
-          longitude: 106.75793529493423,
+          latitude: home.latitude,
+          longitude: home.longitude,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421
         }}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
         zoomControlEnabled={true}
-        provider="google"
+        provider={PROVIDER_GOOGLE}
       >
-        <Marker coordinate={{ latitude: region.latitude, longitude: region.longitude }} />
         <Marker
-          coordinate={pin}
+          coordinate={home}
+        >
+          <Callout><Text>home</Text></Callout>
+        </Marker>
+        <Marker
+          coordinate={company}
           pinColor="black"
           draggable={true}
           onDragStart={(e) => {
             console.log("Drag start", e.nativeEvent.coordinates)
           }}
           onDragEnd={(e) => {
-            setPin({
-              latitude: e.nativeEvent.coordinate.latitude,
-              longitude: e.nativeEvent.coordinate.longitude
-            })
+            setCompany(
+              {
+                latitude: e.nativeEvent.coordinate.latitude,
+                longitude: e.nativeEvent.coordinate.longitude
+              })
           }}
         >
           <Callout>
             <Text>location</Text>
           </Callout>
         </Marker>
-        {/* <Circle center={pin} radius={1000} /> */}
+        <MapViewDirections
+          origin={home}
+          destination={company}
+          apikey='AIzaSyBlduVD6U_I2cpDHlcOWzqIippfMdDSoZ8'
+          strokeWidth={4}
+          strokeColor="#00b0ff"
+        >
+        </MapViewDirections>
       </MapView>
     </View>
   )
@@ -85,13 +149,13 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center"
+    // justifyContent: "center"
   },
   map: {
-    flex: 1
+    width: '100%',
+    height: '100%'
   }
 })
-
